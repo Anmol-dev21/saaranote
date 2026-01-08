9a6004f5a820457c55a5a85b82d057f1a293300c
Anmol Bhargav <anmolbhargav.dev@gmail.com>
2026-01-08 11:01:25 +0000

    Fix integration issues and finalize PDF export feature

--- saaranote_app/lib/presentation/screens/home_screen.dart
+++ saaranote_app/lib/presentation/screens/home_screen.dart
@@ -173,7 +173,8 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           );
         },
-      ),
+            ),
+          ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
